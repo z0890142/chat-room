68786c2b6da6c857c4bd6fd65f40c93be7513490
z0890142 <a0955117097@gmail.com>
2020-09-15 11:10:49 +0800

    add frontend

--- controller/Chat.js
+++ controller/Chat.js
@@ -13,39 +13,31 @@ function ScoketStart(){
 }
 
 io.on('connection', socket => {
-    socket.on('online', function(username){
-        socket.username = username;
-        users[username] = {
-          socketId: socket.id,
-          status: USER_STATUS[0]
-        };
-    })
+  socket.on('online', function(username){
+    socket.emit('getmember',users)
+    socket.username = username;
+    users[username] = {
+      username:username,
+      socketId: socket.id,
+      status: USER_STATUS[0]
+    };
+    console.log(users)
+  })
 
-    socket.on('private_chat', (params, fn) => {
-        const receiver = users[params.receiver];
-        params.createTime = moment().format('YYYY-MM-DD HH:mm:ss');
-        const senderData = _.findWhere(userData, { username: params.sender });
-        params.senderPhoto = (senderData || {}).photo;
-      
-        if (!params.senderPhoto) {
-          const senderLen = params.sender.length;
-          params.senderPhotoNickname = params.sender.substr(senderLen - 2)
-        }
-
-        // fn(params);
-
-        if (receiver && receiver.status === USER_STATUS[0]) {
-          socket.to(users[params.receiver].socketId).emit('reply_private_chat', params);
-        } else {
-          console.log(`${params.receiver} 不在线`);
-          // 可以在做些离线消息推送处理
-        }
-      });
-      
-      socket.on('disconnect', reason => {
-        if (users[socket.username]) users[socket.username].status = USER_STATUS[1];
-      });
+  socket.on('private_chat', (params, fn) => {
+    const receiver = users[params.receiver];
+    if (receiver && receiver.status === USER_STATUS[0]) {
+      socket.to(users[params.receiver].socketId).emit('reply_private_chat', params);
+    } else {
+      console.log(`${params.receiver} offline`);
+    }
   });
+      
+  socket.on('disconnect', reason => {
+    if (users[socket.username]) users[socket.username].status = USER_STATUS[1];
+  });
+});
+
 exports.ChatWithFriend= function(req, res) {
     var user = new Model("User", req.body);	
     user.save(function(error,result) {
--- router/router.js
+++ router/router.js
@@ -2,12 +2,12 @@ const express=require('express');
 const router=express.Router();
 const path=require('path');
 const dirRoot = path.dirname(__dirname);
-const User=require(path.join(dirRoot,'controller','User.js'));
+// const User=require(path.join(dirRoot,'controller','User.js'));
 const Chat=require(path.join(dirRoot,'controller','Chat.js'));
 
 
 
-router.post('/register/user',User.Register);
-router.post('/register/user',Chat.Register);
+// router.post('/register/user',User.Register);
+// router.post('/register/user',Chat.Register);
 
 module.exports=router;
